a02c69938dbe6cb8979e5e1bbe8f8b779a2106a9
Rafael <quirinobytes@gmail.com>
2018-10-13 16:04:46 -0300

    OK

--- servidorPush/public/chat.js
+++ servidorPush/public/chat.js
@@ -1,7 +1,7 @@
 
 $(function(){
    	//make connection
-	var socket = io.connect('http://servidorpush:3000')
+	var socket = io.connect('http://167.99.173.26:3000/')
 
 	//buttons and inputs
 	var message = $("#message")
