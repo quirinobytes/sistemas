bce71a81dca19a9c21b41b3bf621e8fab8305a2a
Rafael <quirinobytes@gmail.com>
2018-10-13 10:04:56 -0300

    OK

--- bEOS/deploy.sh
+++ bEOS/deploy.sh
@@ -7,7 +7,7 @@ cd /root/sistemas/bEOS/deploy
 if [ $? == 0 ]; then
     echo -en "\n\n$atention Deploy done with $green success $normal \n\n"
 	node deploy.js && exit 0 &
-	sleep 3
+	sleep 4
 	DEPLOY_PID=$(ps aux | grep "node deploy.js" | grep -v grep | grep deploy.js | awk '{ print $2} ')
 	kill -9 $DEPLOY_PID
 else
