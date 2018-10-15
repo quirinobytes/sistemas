5f4b61a565f2544987d4573d906d67164916bd4e
Rafael <quirinobytes@gmail.com>
2018-10-15 04:45:02 -0300

    OK

--- servidorPush/servidorPush.js
+++ servidorPush/servidorPush.js
@@ -27,11 +27,12 @@ app.get ('/rest/message/:ativo',function (req,res) {
 	var socketclient = ioclient.connect('http://servidorpush.superati.com.br:3000')
 
 	var ativo = req.params.ativo;
-		html="rafael: " + ativo ;
+		html=ativo;
         res.writeHeader(200, {"Content-Type": "text/html"});
         res.write(html);
         res.end();
-		socketclient.emit('message', {message : ativo , username : socketclient.username});
+		ioclient.sockets.emit('message', {message : ativo , username : socketclient.username});
+		socketclient.disconnect();
 });
 
 
@@ -53,8 +54,8 @@ io.on('connection', (socket) => {
     socket.on('message', (data) => {
         //broadcast the new message
         io.sockets.emit('message', {message : data.message, username : socket.username});
-		if (data.message == "TALI"){
-			io.sockets.emit('message', {message : "Eu acho a TALI linda, beijos", username : socket.username});
+		if (data.message == "deploy"){
+			io.sockets.emit('message', {message : data.message, username : socket.username});
 		}
 		if (data.message == "ntp"){
 			io.sockets.emit('command', {message : "ntpdate ntp.cais.rnp.br", username : socket.username});
